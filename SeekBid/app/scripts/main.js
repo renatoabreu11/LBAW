fd483d13cef6fcb38b823249d13eaf507a561f66
HelderAntunes <helder_antunes-@hotmail.com>
2017-03-03 22:55:15 +0000

    Modal bid added in auction page.

--- SeekBid/app/scripts/main.js
+++ SeekBid/app/scripts/main.js
@@ -73,4 +73,29 @@
   }
 
   // Your custom JavaScript goes here
+
+
+  /**************auction.html***************/
+
+  $(document).on('click', '.number-spinner button', function () {
+    var btn = $(this),
+      oldValue = btn.closest('.number-spinner').find('input').val().trim(),
+      newVal = 0;
+
+    oldValue = oldValue.substring(0, oldValue.length-1);
+
+    if (btn.attr('data-dir') == 'up') {
+      newVal = parseInt(oldValue) + 1;
+    } else {
+      if (oldValue > 1) {
+        newVal = parseInt(oldValue) - 1;
+      } else {
+        newVal = 1;
+      }
+    }
+    btn.closest('.number-spinner').find('input').val(newVal+'$');
+  });
+
 })();
+
+
