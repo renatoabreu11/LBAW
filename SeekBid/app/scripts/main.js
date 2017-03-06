26d3292beeeaf3aee4641f2a6d81c3956f146471
dcepa95 <dcepa95@gmail.com>
2017-03-06 20:47:03 +0000

    Create page still in construction

--- SeekBid/app/scripts/main.js
+++ SeekBid/app/scripts/main.js
@@ -203,4 +203,50 @@
 
 })();
 
+/* ==================== Create Auction ============================
 
+ step script
+ */
+
+$(document).ready(function () {
+  var navListItems = $('div.setup-panel div .btn-circle'),
+    allWells = $('.setup-content'),
+    allNextBtn = $('.nextBtn');
+
+  allWells.hide();
+
+  navListItems.click(function (e) {
+    e.preventDefault();
+    var $target = $($(this).attr('href')),
+      $item = $(this);
+
+    if (!$item.hasClass('disabled')) {
+      navListItems.removeClass('btn-primary').addClass('btn-default');
+      $item.addClass('btn-primary');
+      allWells.hide();
+      $target.show();
+      $target.find('input:eq(0)').focus();
+    }
+  });
+
+  allNextBtn.click(function(){
+    var curStep = $(this).closest(".setup-content"),
+      curStepBtn = curStep.attr("id"),
+      nextStepWizard = $('div.setup-panel div .btn-circle[href="#' + curStepBtn + '"]').parent().next().children(".btn-circle"),
+      curInputs = curStep.find("input[type='text'],input[type='url']"),
+      isValid = true;
+
+    $(".form-group").removeClass("has-error");
+    for(var i=0; i<curInputs.length; i++){
+      if (!curInputs[i].validity.valid){
+        isValid = false;
+        $(curInputs[i]).closest(".form-group").addClass("has-error");
+      }
+    }
+
+    if (isValid)
+      nextStepWizard.removeAttr('disabled').trigger('click');
+  });
+
+  $('div.setup-panel div .btn-circle.btn-primary').trigger('click');
+});
