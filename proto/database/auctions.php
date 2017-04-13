cc1d46c74b1967eb8aa3e5dab0326b0725e97b1d
HelderAntunes <helder_antunes-@hotmail.com>
2017-04-13 18:33:38 +0100

    best_auctions page updated.

--- proto/database/auctions.php
+++ proto/database/auctions.php
@@ -1,14 +1,17 @@
 <?php
-	
+
+/**
+ * Popular = more bids
+ */
 function getMostPopularAuctions() {
     global $conn;
-    $stmt = $conn->prepare('SELECT auction.id, product.name, "user".username, "user".rating, auction.curr_bid, auction.end_date
+    $stmt = $conn->prepare('SELECT auction.id, product.name as product_name, "user".username, "user".rating as user_rating, auction.curr_bid, auction.end_date, "user".id as user_id
                             FROM bid
                             INNER JOIN auction ON bid.auction_id = auction.id
                             INNER JOIN product ON auction.product_id = product.id
                             INNER JOIN "user" ON auction.user_id = "user".id
                             WHERE now() < auction.end_date
-                            GROUP BY auction.id, product.name, "user".username, "user".rating, auction.curr_bid, auction.end_date
+                            GROUP BY auction.id, product.name, "user".username, "user".rating, auction.curr_bid, auction.end_date, "user".id
                             ORDER BY COUNT(*) DESC
                             LIMIT 15;');
     $stmt->execute();
--- proto/pages/auctions/best_auctions.php
+++ proto/pages/auctions/best_auctions.php
@@ -1,16 +1,42 @@
 <?php
-  include_once ('../../config/init.php');
-  include_once ($BASE_DIR . 'database/auctions.php');
-  include_once ($BASE_DIR . 'database/users.php');
-  
-  $auctions = getMostPopularAuctions();
-  $numActiveAuctions = getNumActiveAuctions();
-  $totalValOfActiveAuctions = getTotalValueOfActiveAuctions();
-  $topTenRankingUsers = getTopTenRankingUsers();
+	include_once ('../../config/init.php');
+	include_once ($BASE_DIR . 'database/auctions.php');
+	include_once ($BASE_DIR . 'database/users.php');
+	include_once ($BASE_DIR . 'database/admins.php');
 
-  $smarty->assign('auctions', $auctions);
-  $smarty->assign('numActiveAuctions', $numActiveAuctions);
-  $smarty->assign('totalValOfActiveAuctions', $totalValOfActiveAuctions);
-  $smarty->assign('topTenRankingUsers', $topTenRankingUsers);
-  $smarty->display('auctions/list_best_auctions.tpl');
+	$categories = getCategories();
+	$numActiveAuctions = getNumActiveAuctions();
+	$totalValOfActiveAuctions = getTotalValueOfActiveAuctions();
+	$topTenRankingUsers = getTopTenRankingUsers();
+	$mostPopularAuctions = getMostPopularAuctions();
+	
+	$now  = strtotime(date('Y-m-d H:i:s'));
+	foreach ($mostPopularAuctions as &$auction) {
+
+		$endDate = strtotime($auction['end_date']);
+		$differenceInSeconds = $endDate - $now;
+		
+		$diffHours = intval($differenceInSeconds /(60 * 60));
+		$diffMinutes = intval(($differenceInSeconds-$diffHours*60*60)/60);
+		$diffSeconds = intval($differenceInSeconds-$diffHours*60*60-$diffMinutes*60);
+
+		$diffHours = (string)$diffHours;
+		$diffMinutes = (string)$diffMinutes;
+		$diffSeconds = (string)$diffSeconds;
+		
+		if (strlen($diffHours) == 1) $diffHours = '0'.$diffHours;
+		if (strlen($diffMinutes) == 1) $diffMinutes = '0'.$diffMinutes;
+		if (strlen($diffSeconds) == 1) $diffSeconds = '0'.$diffSeconds;
+
+		$timeRemaining = $diffHours."h:".$diffMinutes."m:".$diffSeconds."s";
+
+		$auction['time_remain'] = $timeRemaining;
+	}
+
+	$smarty->assign('categories', $categories);
+	$smarty->assign('numActiveAuctions', $numActiveAuctions);
+	$smarty->assign('totalValOfActiveAuctions', $totalValOfActiveAuctions);
+	$smarty->assign('topTenRankingUsers', $topTenRankingUsers);
+	$smarty->assign('mostPopularAuctions', $mostPopularAuctions);
+	$smarty->display('auctions/list_best_auctions.tpl');
 ?>
