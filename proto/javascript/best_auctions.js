bdf489fe35c73012e60cc0672e4e166f7076fb17
HelderAntunes <helder_antunes-@hotmail.com>
2017-04-14 12:07:47 +0100

    countdown in best auctions done.

---
+++ proto/javascript/best_auctions.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	$('.clock').each(function() {
+		var date = $(this).find('p').text();
+   		$(this).countdown(date)
+		.on('update.countdown', function(event) {
+		  var format = '%Hh:%Mm:%Ss';
+		  if(event.offset.totalDays > 0) {
+		    format = '%-d day%!d ' + format;
+		  }
+		  if(event.offset.weeks > 0) {
+		    format = '%-w week%!w ' + format;
+		  }
+		  $(this).html(event.strftime(format));
+		})
+		.on('finish.countdown', function(event) {
+		  $(this).html('This offer has expired!')
+		    .parent().addClass('disabled');
+
+		});
+	});
+    
+});
--- proto/pages/auctions/best_auctions.php
+++ proto/pages/auctions/best_auctions.php
@@ -10,29 +10,6 @@
 	$topTenRankingUsers = getTopTenRankingUsers();
 	$mostPopularAuctions = getMostPopularAuctions();
 	
-	$now  = strtotime(date('Y-m-d H:i:s'));
-	foreach ($mostPopularAuctions as &$auction) {
-
-		$endDate = strtotime($auction['end_date']);
-		$differenceInSeconds = $endDate - $now;
-		
-		$diffHours = intval($differenceInSeconds /(60 * 60));
-		$diffMinutes = intval(($differenceInSeconds-$diffHours*60*60)/60);
-		$diffSeconds = intval($differenceInSeconds-$diffHours*60*60-$diffMinutes*60);
-
-		$diffHours = (string)$diffHours;
-		$diffMinutes = (string)$diffMinutes;
-		$diffSeconds = (string)$diffSeconds;
-		
-		if (strlen($diffHours) == 1) $diffHours = '0'.$diffHours;
-		if (strlen($diffMinutes) == 1) $diffMinutes = '0'.$diffMinutes;
-		if (strlen($diffSeconds) == 1) $diffSeconds = '0'.$diffSeconds;
-
-		$timeRemaining = $diffHours."h:".$diffMinutes."m:".$diffSeconds."s";
-
-		$auction['time_remain'] = $timeRemaining;
-	}
-
 	$smarty->assign('categories', $categories);
 	$smarty->assign('numActiveAuctions', $numActiveAuctions);
 	$smarty->assign('totalValOfActiveAuctions', $totalValOfActiveAuctions);
