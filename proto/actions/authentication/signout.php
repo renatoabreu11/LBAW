a8cdfae2db47903c20d2ec26b437040397aa2608
renatoabreu11 <renatoabreu1196@gmail.com>
2017-04-19 18:33:22 +0100

    minor fix

--- proto/actions/authentication/signout.php
+++ proto/actions/authentication/signout.php
@@ -2,10 +2,6 @@
 
 include_once('../../config/init.php');
 
-if($username != null)
-    $username = null;
-if($password != null)
-    $password = null;
 if($_SESSION['username'] != null)
     unset($_SESSION['username']);
 if($_SESSION['admin_username'] != null)
