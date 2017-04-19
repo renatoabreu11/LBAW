daaa9feb6f05ee2a7e80a0d9dcd186857fd8daa4
renatoabreu11 <renatoabreu1196@gmail.com>
2017-04-19 18:56:22 +0100

    logout minor fix

--- proto/actions/authentication/signout.php
+++ proto/actions/authentication/signout.php
@@ -6,5 +6,7 @@ if($_SESSION['username'] != null)
     unset($_SESSION['username']);
 if($_SESSION['admin_username'] != null)
     unset($_SESSION['admin_username']);
+if($_SESSION['user_id'] != null)
+    unset($_SESSION['user_id']);
 
 header("Location: $BASE_URL" . 'index.php');
