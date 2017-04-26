270256c8f387d19949a36eeae2fa1596359f8930
Evenilink <jose__carlos17@hotmail.com>
2017-04-26 19:34:50 +0100

    auction page more completed.

--- proto/pages/auction/auction.php
+++ proto/pages/auction/auction.php
@@ -7,6 +7,24 @@ $auction_id = $_GET["id"];
 $auction = getAuction($auction_id);
 $product = getAuctionProduct($auction_id);
 
+$seller = getUser($auction['user_id']);
+$numReviews = count(getReviews($Auction['user-id']));
+
+$recentBidders = getRecentBidders($auction_id);
+$numBids = getTotalNumBids($auction_id);
+$numBidders = count(getBidders($auction_id));
+
+if(date('Y-m-d H:i:s') > $auction.end_date) {
+    $winningUser = getWinningUser($auction_id);
+    $smarty->assign("winningUser", $winningUser);
+}
+
 $smarty->assign("product", $product);
 $smarty->assign("auction", $auction);
+$smarty->assign("seller", $seller);
+$smarty->assign("numReviews", $numReviews);
+$smarty->assign("recentBidders", $recentBidders);
+$smarty->assign("numBids", $numBids);
+$smarty->assign("numBidders", $numBidders);
+
 $smarty->display('auction/auction.tpl');
