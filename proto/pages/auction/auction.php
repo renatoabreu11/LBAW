ef4fab0bce1358f9af1ba0bf5738a704890cc59b
Evenilink <jose__carlos17@hotmail.com>
2017-04-29 14:19:56 +0100

    auction page ending time showing

--- proto/pages/auction/auction.php
+++ proto/pages/auction/auction.php
@@ -6,6 +6,7 @@ include_once ($BASE_DIR . 'database/users.php');
 
 $auction_id = $_GET["id"];
 $auction = getAuction($auction_id);
+$auction['end_data_readable'] = date('d F Y, H:i:s', strtotime($auction['end_date']));
 $product = getAuctionProduct($auction_id);
 
 $seller = getUser($auction['user_id']);
