bdd7309467549e537641a57c7bb51253734868f9
Evenilink <jose__carlos17@hotmail.com>
2017-05-07 00:01:49 +0100

    resize image in user-edit

--- proto/actions/user/user_edit.php
+++ proto/actions/user/user_edit.php
@@ -3,6 +3,8 @@
 include_once('../../config/init.php');
 include_once($BASE_DIR . 'database/users.php');
 
+use Intervention\Image\ImageManager;
+
 if (!$_POST['token'] || !hash_equals($_SESSION['token'], $_POST['token'])) {
   $_SESSION['error_messages'][] = "You don't have permissions to make this request. Generated token is different.";
   header("Location:"  . $BASE_URL);
@@ -101,10 +103,11 @@ if($picture['size'] > 0) {
     exit;
   }
 
-  //$img = resize_image($picturePath, 200, 200);
-  
-  // and you are ready to go ...
-  //$image = Image::make($picturePath)->resize(300, 200);
+  // Resize the image.
+  $manager = new ImageManager();
+  $img = $manager->make($picturePath);
+  $img->fit(156, 156);
+  $img->save($picturePath);
 
   try {
     updateUserPicture($userId, $userId . "." . $extension);
@@ -124,30 +127,3 @@ if($picture['size'] > 0) {
 
 $_SESSION['success_messages'][] = 'Personal information updated with success.';
 header("Location: $BASE_URL" . 'pages/user/user.php?id=' . $userId);
-
-function resize_image($file, $w, $h, $crop=FALSE) {
-    list($width, $height) = getimagesize($file);
-    $r = $width / $height;
-    if ($crop) {
-        if ($width > $height) {
-            $width = ceil($width-($width*abs($r-$w/$h)));
-        } else {
-            $height = ceil($height-($height*abs($r-$w/$h)));
-        }
-        $newwidth = $w;
-        $newheight = $h;
-    } else {
-        if ($w/$h > $r) {
-            $newwidth = $h*$r;
-            $newheight = $h;
-        } else {
-            $newheight = $w/$r;
-            $newwidth = $w;
-        }
-    }
-    $src = imagecreatefromjpeg($file);
-    $dst = imagecreatetruecolor($newwidth, $newheight);
-    imagecopyresampled($dst, $src, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
-
-    return $dst;
-}
